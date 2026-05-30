3ee84c52e65512bb0825ba693757b8bb30e2e1ca
lzw0796 <lzw0796@gmail.com>
2026-05-29 18:23:54 -0600

    Refactor XH_Plugins_Custom for improved clarity

--- xunhu-plugins-custom.js
+++ xunhu-plugins-custom.js
@@ -1,13 +1,7 @@
 (function ($) {
     window.XH_Plugins_Custom={
-        /**
-         * 每次打开弹窗都会清空
-         */
         data:{},
-        /**
-         * 打开立即购买弹窗
-         * @param params ajax地址、当前文章地址、当前文章postID
-         */
+
         xh_now_pay_model_show:function (params) {
             if(params.modal){
                 switch (params.modal){
@@ -19,62 +13,51 @@
                         break;
                     default:
                         XH_Plugins_Custom.data={};
+                        window.xh_pay_lock = false;
                         $('#xh_now_pay_modal').show();
                         XH_Plugins_Custom.__ajax(params,XH_Plugins_Custom.bind_url);
                 }
             }
         },
-        /**
-         * 关闭弹窗
-         */
+
         close_model:function () {
             $(".xunhu-modal").each(function(){
                 $(this).hide();
             });
         },
-        /**
-         * 该url用于携带支付方式，请求支付链接
-         */
+
         bind_url:function(_data) {
             XH_Plugins_Custom.data['url']=_data['url'];
         },
-        /**
-         * 弹窗+微信支付宝扫码
-         */
+
         show_model_qrcode:function(data){
-            //价格
             $("#xh_now_pay_modal_2 .modal-price").html(data.price_html);
-            //二维码
             $("#xh_now_pay_modal_2_qrcode").attr("src", data.qrcode_url);
             $('#xh_now_pay_modal_2').show();
             console.log(data);
         },
-        /**
-         * 重定向到购物车页面
-         */
+
         redirect_cart:function (data) {
             location.href=data;
         },
-        /**
-         * 打开购买会员弹窗
-         */
+
         xh_buy_membership_model_show:function () {
             XH_Plugins_Custom.data={
-                buy_membership:'pay_mode'   //打开弹窗默认的购买会员的方式为付款
+                buy_membership:'pay_mode'
             };
-            //设置默认选择的会员类型
+
             $('.xh-membership-type').each(function () {
                 if($(this).is('.active')){
                     XH_Plugins_Custom.data['membership_id']=$(this).data('id');
                 }
             });
+
             $('#xh_buy_membership_model').show();
         },
-        /**
-         * 显示或隐藏会员说明
-         */
+
         show_or_hide_membership_notes:function (obj,containerID) {
             var currentNode=$('#'+containerID);
+
             if($(obj).hasClass('xunhu-up-icon')){
                 $(obj).removeClass('xunhu-up-icon');
                 $(obj).addClass('xunhu-down-icon');
@@ -82,65 +65,86 @@
                 $(obj).removeClass('xunhu-down-icon');
                 $(obj).addClass('xunhu-up-icon');
             }
-            //显示 隐藏
+
             if(currentNode.is(':hidden')){
                 currentNode.show();
             }else{
                 currentNode.hide();
             }
         },
-        /**
-         * 会员类型选择
-         */
+
         xh_membership_type_sel:function (currentObj) {
             $('.xh-membership-type').each(function () {
                 $(this).removeClass('active');
             });
+
             $(currentObj).addClass('active');
             XH_Plugins_Custom.data['membership_id']=$(currentObj).data('id');
         },
-        /**
-         * 购买会员方式选择
-         */
+
         xh_buy_membership_mode_sel:function (obj) {
             var showModeID=$(obj).attr('id');
             var hideModeID='code_mode';
+
             if(showModeID==='code_mode'){
                 hideModeID='pay_mode';
             }
+
             var showModeContainerID=showModeID+'_container';
             var hideModeContainerID=hideModeID+'_container';
+
             XH_Plugins_Custom.show_buy_membership_mode(showModeID,showModeContainerID);
             XH_Plugins_Custom.hide_buy_membership_mode(hideModeID,hideModeContainerID);
-            //设置当前购买会员的方式
+
             XH_Plugins_Custom.data['buy_membership']=showModeID;
         },
-        /**
-         * 显示会员购买方式
-         */
+
         show_buy_membership_mode:function (showModeID,showModeContainerID) {
             if(!($('#'+showModeID).is('.active'))){
                 $('#'+showModeID).addClass('active');
             }
+
             $('#'+showModeContainerID).show();
         },
-        /**
-         * 隐藏会员购买方式
-         */
+
         hide_buy_membership_mode:function (hideModeID,hideModeContainerID) {
             $('#'+hideModeID).removeClass('active');
             $('#'+hideModeContainerID).hide();
         },
-        /**
-         * 立即支付
-         * source 来源，可能来源于立即购买的弹窗，也可能来源于升级会员的弹窗
-         */
+
         xh_now_pay:function(source,params){
             if(!source)return;
+
             switch (source){
                 case 'xh_now_pay_modal':
+
                     if(!XH_Plugins_Custom.data['url']) return;
+
+                    if(window.xh_pay_lock){
+                        return false;
+                    }
+
+                    window.xh_pay_lock = true;
+
+                    var $btn = $("#xh_now_pay_modal .btn-success");
+
+                    if(!$btn.length){
+                        $btn = $("#xh_now_pay_modal button, #xh_now_pay_modal a").filter(function(){
+                            return ($(this).text() || '').indexOf('立即支付') !== -1;
+                        }).first();
+                    }
+
+                    $btn.attr('data-old-text', $btn.text());
+                    $btn.text('正在跳转>>>');
+                    $btn.prop('disabled', true);
+                    $btn.css({
+                        opacity:'0.6',
+                        cursor:'not-allowed',
+                        pointerEvents:'none'
+                    });
+
                     var payment=$('#xh_now_pay_modal input:radio[name="payment"]:checked').val();
+
                     var _params={
                         url:XH_Plugins_Custom.data['url'],
                         type:'POST',
@@ -148,29 +152,62 @@
                             payment_method:payment
                         }
                     };
-                    //提交订单，进行支付
+
                     XH_Plugins_Custom.__ajax(_params,XH_Plugins_Custom.pay);
                     break;
+
                 case 'xh_buy_membership_model':
-                    //提交订单，进行支付
+
+                    if(window.xh_pay_lock){
+                        return false;
+                    }
+
+                    window.xh_pay_lock = true;
+
                     XH_Plugins_Custom.__ajax(params,XH_Plugins_Custom._pay);
                     break;
             }
         },
+
         _pay:function (_data) {
-            XH_Plugins_Custom.pay(_data['redirect_url'])
+            XH_Plugins_Custom.pay(_data['redirect_url']);
         },
-        /**
-         * 进行支付
-         */
+
         pay:function (_data) {
+            window.xh_pay_lock = false;
             location.href=_data;
         },
+
+        reset_pay_button:function(){
+            window.xh_pay_lock = false;
+
+            var $btn = $("#xh_now_pay_modal .btn-success");
+
+            if(!$btn.length){
+                $btn = $("#xh_now_pay_modal button, #xh_now_pay_modal a").filter(function(){
+                    return ($(this).text() || '').indexOf('正在跳转') !== -1;
+                }).first();
+            }
+
+            if($btn.length){
+                var oldText = $btn.attr('data-old-text') || '立即支付';
+
+                $btn.text(oldText);
+                $btn.prop('disabled', false);
+                $btn.css({
+                    opacity:'',
+                    cursor:'',
+                    pointerEvents:''
+                });
+            }
+        },
+
         __ajax:function (params,callback) {
             if(!params) return;
             if(!params.url) return;
             if(!params.type) params.type='POST';
             if(!params.data) params.data={};
+
             $.ajax({
                 url: params.url,
                 type: params.type,
@@ -181,15 +218,23 @@
                 success: function(res) {
                     if(res.errcode!=0){
                         console.log(res);
+
+                        XH_Plugins_Custom.reset_pay_button();
+
                         if(res.errcode!==501){
                             $('#xh_buy_model_error_notice').html(res.errmsg);
                         }
+
                         $('#xh_buy_model_error_notice').show();
                         return;
                     }
+
                     callback(res.data);
-                },error:function(e){
+                },
+                error:function(e){
                     console.log(e);
+                    XH_Plugins_Custom.reset_pay_button();
+                    alert('支付请求失败，请稍后重试');
                 }
             });
         }
